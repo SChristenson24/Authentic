76dfea78f9eb9f205a7bb80fb45a3cc0e7a1b1ed
John Mark Taylor <157426285+jmt880@users.noreply.github.com>
2024-10-01 14:22:28 -0500

    Sign Out button now working

--- Authentic/Authentic/Authentic/Authentic/SuccessView.swift
+++ Authentic/Authentic/Authentic/Authentic/SuccessView.swift
@@ -1,23 +1,43 @@
 //
 //  SuccessView.swift
-//  Authentic
-//
-//  Created by John Mark Taylor on 9/24/24.
-//
 import SwiftUI
 
 struct SuccessView: View {
+    @Environment(\.presentationMode) var presentationMode
+    
     var body: some View {
         ZStack {
             // Background Color (Pink)
             Color.pink
                 .ignoresSafeArea() // Fills the entire screen
             
-            // Text in the center
-            Text("You are successfully signed in")
-                .font(.title) // Font size
-                .foregroundColor(.black) // Text color
-                .padding()
+            // Content
+            VStack {
+                Text("You are successfully signed in")
+                    .font(.title) // Font size
+                    .foregroundColor(.black) // Text color
+                    .padding()
+                
+                // Logout Button
+                Button(action: {
+                    Task {
+                        do {
+                            try await AuthenticationManager.shared.signOut()
+                            presentationMode.wrappedValue.dismiss()
+                        } catch {
+                            print("Error signing out: \(error)")
+                        }
+                    }
+                }) {
+                    Text("Log Out")
+                        .font(.headline) // Font size
+                        .padding()
+                        .background(Color.black) // Background color for the button
+                        .foregroundColor(.white) // Text color
+                        .cornerRadius(10) // Rounded corners
+                }
+            }
+            .padding(.top, 20) // Space above the button
         }
     }
 }
